abe421efe608def25e2a58d4d0562a9d611a20e6
sylank <serleg.dani@live.com>
2018-09-19 23:58:16 +0200

    typos

--- api_gateway_module/main.tf
+++ api_gateway_module/main.tf
@@ -5,10 +5,6 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [
-    "aws_api_gateway_integration.integration",
-  ]
-
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.stage_name}"
 
