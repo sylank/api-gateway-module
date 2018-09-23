e5fb31540942948f4d1dc9fff1bcfb01ec3385ac
sylank <serleg.dani@live.com>
2018-09-23 16:08:16 +0200

    deployment added

--- api_gateway_module/main.tf
+++ api_gateway_module/main.tf
@@ -4,14 +4,14 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "${var.api_description}"
 }
 
-/*resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.stage_name}"
 
   variables = {
     "deployed_at" = "${var.deployed_at}"
   }
-}*/
+}
 
 resource "aws_api_gateway_resource" "root_resource" {
   path_part   = "${var.root_path}"
