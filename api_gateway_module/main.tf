138c151ceb65b84edcce8e7a7792d5dccc6de084
sylank <serleg.dani@live.com>
2018-09-30 12:57:52 +0200

    test

--- api_gateway_module/main.tf
+++ api_gateway_module/main.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "api" {
   name        = "${var.api_name}"
   description = "${var.api_description}"
 }
-
+/*
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.stage_name}"
@@ -11,7 +11,7 @@ resource "aws_api_gateway_deployment" "deployment" {
   variables = {
     "deployed_at" = "${var.deployed_at}"
   }
-}
+}*/
 
 resource "aws_api_gateway_resource" "root_resource" {
   path_part   = "${var.root_path}"
